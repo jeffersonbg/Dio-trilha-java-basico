6f56a14c62a5e5ddf055ba5e4ecba6fd5e21d7c0
Jefferson Bezerra da Gama <jeffersonbg.ufersa@gmail.com>
2024-05-08 00:31:48 -0300

    Finalização do Projeto Desafio DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo = 237.48;
+
+        System.out.println("Bem vindo ao Banco Terminal.");
+        System.out.println();
+        System.out.print("Digite o número da conta: ");
+        numero = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Agência: ");
+        agencia = scan.nextLine();
+        System.out.print("Nome do cliente: ");
+        nomeCliente = scan.nextLine();
+        System.out.println();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scan.close();
+
     }
 }
